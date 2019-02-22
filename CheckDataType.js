f9bb954d9dabd05c8490af0c16b2590f55764735
devesh166 <jiodevesh@gmail.com>
2019-02-22 17:39:37 +0530

    Update CheckDataType.js
    debug1

--- CheckDataType.js
+++ CheckDataType.js
@@ -1,4 +1,4 @@
-function checktype(x){
+ checktype =(x)=>{
     
     if(x == null && typeof x == "object"){
         return "This is null"
@@ -13,29 +13,66 @@ function checktype(x){
    }
     
   
-if(x.constructor ==Array){
-    if(x[1].constructor== Object){
+else if(x.constructor ==Array ){
+    if(x[0].constructor== Object){
         console.log("This is an array of object :")  
-    }else if(x[1].constructor== String){
+        console.log("[")
+      x.map( function(f,i){
+        var keys = Object.keys(f);
+        // value = [];
+         var values = Object.values(f);
+         console.log("{")
+         keys.map( (v,i)=>{
+            console.log(v + ":" + values[i]);
+    
+        })
+         console.log("}")
+         //console.log(",")
+     })
+     console.log("]")
+
+    }else if(x[0].constructor== String){
         console.log("This is an array of String :")  
     }else{
         console.log("This is an array  :");
+        
+        console.log(x.map((v)=>{
+            console.log(v);
+        }))
+
+
     }
+
+      
 }
 else if(x.constructor ==String){
     console.log("This is an String :");
+    return x;
 }
 else if(x.constructor ==Number){
     console.log("This is an Number:");
+    return x;
 }
 else if(x.constructor ==Object){
-    console.log("This is an Object :");
+     console.log("This is an Object :");
+     var keys = Object.keys(x);
+    // value = [];
+     var values = Object.values(x);
+     console.log("{")
+     keys.map( (v,i)=>{
+        console.log(v + ":" + values[i]);
+
+    })
+     console.log("}")
+
 }
 else if(x.constructor ==Function){
     console.log("This is a Function :");
+    return x;
 }
 else if(x.constructor ==Boolean){
     console.log("This is a Boolean :");
+    return x;
 }
 
 
@@ -45,8 +82,9 @@ else{
 
 
 
-return x;
+
 
 }
 
 checktype( )
+ 
